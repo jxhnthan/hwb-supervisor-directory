1b59f380d0cbd38209c94946959c6b2b2216bc1d
Johnathan <85273325+jxhnthan@users.noreply.github.com>
2025-07-10 20:08:50 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const sidebarBadges: Badge[] = [
   { id: 'badge-pet', src: "/images/PET.png", alt: "Pets Badge" },
   { id: 'badge-eagle', src: "/images/EAGLE.png", alt: "Eagle Badge" },
   { id: 'badge-pool', src: "/images/POOL.png", alt: "Pool Badge" },
-  { id: 'badge-penguin', src: "/images/PI.png", alt: "Penguin Badge" },
+  { id: 'badge-penguin', src: "/images/PI.png", alt: "Penguin Icecream Badge" },
   { id: 'badge-corgi', src: "/images/DANCE.png", alt: "Dancing Badge" },
   { id: 'badge-dance', src: "/images/CORGI.png", alt: "Corgi Badge" },
 { id: 'badge-fc', src: "/images/FC.png", alt: "Fortune Cookie Badge" }
